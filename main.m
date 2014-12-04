8ea239cb2a89b6dbff4e8716795afee9b70ab0da
Konstantin Käfer <mail@kkaefer.com>
2014-12-04 18:10:53 +0100

    initial commit

---
+++ main.m
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    system("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend");
+    return 0;
+}
